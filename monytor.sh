4ca5b9073758d48720ef5dfa90bd3f19159190b1
vintagefuture <pinkpallyn@gmail.com>
2024-11-15 15:11:55 +0000

    adding -L flag to curl for redirect

--- monytor.sh
+++ monytor.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
 set -a
-source ./.env
+source "${SCRIPT_DIR}/.env"
 set +a
 
 URL='https://docs.paloaltonetworks.com/compatibility-matrix/globalprotect/where-can-i-install-the-globalprotect-app'
-KEYWORD='sonoma'
+KEYWORD='sequoia'
 NTFY_SERVER='https://ntfy.542646.xyz'
 NTFY_TOPIC='paloalto'
 
 function notify {
-	curl -u ${NTFY_USER}:${NTFY_PWD} -d "Mentioned the keyword $1" ${NTFY_SERVER}/${NTFY_TOPIC}
+	curl -u :tk_1dhk6g338j3c1n69hhooe48f2l966 -d "Mentioned the keyword $1" ${NTFY_SERVER}/${NTFY_TOPIC}
 }
 
-if curl -s "$URL" | grep -qi "$KEYWORD"; then
+if curl -sL "$URL" | grep -qi "$KEYWORD"; then
     echo "Keyword '$KEYWORD' found"
     notify "$KEYWORD"
 fi
